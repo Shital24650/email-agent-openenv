a73a5d9c1f18d6aca40ebfcb3fc45a08bafd4fc8
Shital24650 <shitalparab2605@gmail.com>
2026-04-11 21:04:10 +0530

    Modify grader reference in TASK dictionary
    Updated the grader reference in the TASK dictionary.

--- tasks/task3.py
+++ tasks/task3.py
@@ -1,7 +1,5 @@
-from grader import grade_step
-
 TASK = {
     "id": "hard_full_action",
     "description": "Perform full email handling",
-    "grader": grade_step,
+    "grader": "grader:grade_step",
 }

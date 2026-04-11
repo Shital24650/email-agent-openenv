0a4e88c46eb44b8fa48658edd7364cc519415920
Shital24650 <shitalparab2605@gmail.com>
2026-04-11 21:03:26 +0530

    Update task1.py

--- tasks/task1.py
+++ tasks/task1.py
@@ -1,7 +1,5 @@
-from grader import grade_step
-
 TASK = {
     "id": "easy_classification",
     "description": "Classify email category correctly",
-    "grader": grade_step,
+    "grader": "grader:grade_step",
 }
